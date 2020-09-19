56bb4a977c698859fecbf3d18276df0ef1467661
Galaraz <jvictor.si.2013@gmail.com>
2020-09-19 09:52:45 -0300

    cadastro de clientes

---
+++ 03-cadastro-cliente/js/script.js
@@ -0,0 +1,114 @@
+function fecharMenu() {
+  document.getElementById("menu").classList.add("fechar-menu")
+  document.getElementById("main").classList.add("aumentar-main")
+  document.getElementById("btn-menu").classList.add("btn-menu-abrir-exibir")
+  document.getElementById("menu").classList.add("fechar-menu");
+  document.getElementById("main").classList.add("aumentar-main");
+  document.getElementById("btn-menu").classList.add("btn-menu-abrir-exibir");
+}
+
+function abrirMenu() {
+  document.getElementById("menu").classList.remove("fechar-menu")
+  document.getElementById("main").classList.remove("aumentar-main")
+  document.getElementById("btn-menu").classList.remove("btn-menu-abrir-exibir")
+  document.getElementById("menu").classList.add("exibir-menu")
+  document.getElementById("menu").classList.remove("fechar-menu");
+  document.getElementById("main").classList.remove("aumentar-main");
+  document.getElementById("btn-menu").classList.remove("btn-menu-abrir-exibir");
+  document.getElementById("menu").classList.add("exibir-menu");
+}
+
+function validar() {
+
+  // Leitura dos dados
+
+  let nome = document.getElementById("nome").value
+  let email = document.getElementById("email").value
+  let sexo = document.querySelector("[type=radio]:checked")
+  let cursos = document.querySelector("[type=checkbox]:checked")
+  let estado = document.getElementById("estado").value
+  let foto = document.getElementById("foto").files
+  let dataNasc = document.getElementById("datanasc").value
+
+  // Comparações
+  if (nome == "") {
+    alert("Campo nome é obrigatório!!!")
+  }
+
+  if (email == "") {
+    alert("Campo e-mail é obrigatório!!!")
+  }
+
+  if (sexo == null) {
+    alert("Selecione o sexo!!!")
+  }
+
+  if (cursos == null) {
+    alert("Selecione pelo menos um curso!!!")
+  }
+
+  if (estado == "") {
+    alert("Selecione o estado!!!")
+  }
+
+  if (foto.length == 0) {
+    alert("Selecione a foto!!!")
+  }
+
+  if (dataNasc == "") {
+    alert("Selecione a data de nascimento!!!")
+  }
+  function fecharMensagem() {
+    document.getElementById("mensagens").classList.remove("show");
+  }
+
+}
+function validar() {
+  // Leitura dos dados
+  let nome = document.getElementById("nome").value;
+  let email = document.getElementById("email").value;
+  let sexo = document.querySelector("[type=radio]:checked");
+  let cursos = document.querySelector("[type=checkbox]:checked");
+  let estado = document.getElementById("estado").value;
+  let foto = document.getElementById("foto").files;
+  let dataNasc = document.getElementById("datanasc").value;
+
+  let mensagem = "";
+
+  // Comparações
+  if (nome == "") {
+    mensagem += "Campo nome é obrigatório!!!\n";
+  }
+
+  if (email == "") {
+    mensagem += "Campo e-mail é obrigatório!!!\n";
+  }
+
+  if (sexo == null) {
+    mensagem += "Selecione o sexo!!!\n";
+  }
+
+  if (cursos == null) {
+    mensagem += "Selecione pelo menos um curso!!!\n";
+  }
+
+  if (estado == "") {
+    mensagem += "Selecione o estado!!!\n";
+  }
+
+  if (foto.length == 0) {
+    mensagem += "Selecione a foto!!!\n";
+  }
+
+  if (dataNasc == "") {
+    mensagem += "Selecione a data de nascimento!!!\n";
+  }
+
+  if (mensagem != "") {
+    document.getElementById("textoMensagem").innerText = mensagem;
+    document.getElementById("mensagens").classList.add("show");
+  }
+}
+
+
+
